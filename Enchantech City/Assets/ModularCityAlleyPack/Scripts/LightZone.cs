6d8354c74db123005778d279755b153a38865824
Andrei <amborn02@gmail.com>
2025-09-29 14:44:34 -0500

    Halloween stuff and quest progress

---
+++ Enchantech City/Assets/ModularCityAlleyPack/Scripts/LightZone.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightZone: MonoBehaviour
+{
+    [SerializeField] Material shopOnMaterial;
+    [SerializeField] Material shopOffMaterial;
+
+    [SerializeField] Material streetOnMaterial;
+    [SerializeField] Material streetOffMaterial;
+
+    [SerializeField] GameObject[] objects;
+    void Start()
+    {
+        TurnLightsOff();
+    }
+
+    public void TurnLightsOn()
+    {
+        foreach (var obj in objects)
+        {
+            MeshRenderer mr;
+
+            obj.TryGetComponent(out mr);
+
+            if (mr != null)
+            {
+                for (int i = 0; i < mr.materials.Length; i++)
+                {
+                    if (mr.materials[i].name == shopOffMaterial.name)
+                    {
+                        mr.materials[i] = shopOnMaterial;
+                    } else if (mr.materials[i].name == streetOffMaterial.name)
+                    {
+                        mr.materials[i] = streetOnMaterial;
+                    }
+                }
+            }
+        }
+    }
+
+    public void TurnLightsOff()
+    {
+        foreach (var obj in objects)
+        {
+            MeshRenderer mr;
+
+            obj.TryGetComponent(out mr);
+
+            if (mr != null)
+            {
+                for (int i = 0; i < mr.materials.Length; i++)
+                {
+                    if (mr.materials[i].name == shopOnMaterial.name)
+                    {
+                        mr.materials[i] = shopOffMaterial;
+                    }
+                    else if (mr.materials[i].name == streetOnMaterial.name)
+                    {
+                        mr.materials[i] = streetOffMaterial;
+                    }
+                }
+            }
+        }
+    }
+}
